6971ff92f47ae2f8a099c5b19ca2f86702a23d9b
YangSoeun <soeun8636@naver.com>
2024-06-28 16:52:29 +0900

    [FEAT]: Transaction 클래스 생성

---
+++ bankAccount/src/domain/Transaction.java
@@ -0,0 +1,9 @@
+package domain;
+
+public class Transaction {
+    String transactionDate;
+    String transactionTime;
+    String kind;   //입출금
+    long amount;   //거래금액
+    long balance;   //잔고
+}
