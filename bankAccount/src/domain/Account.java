18f9f0e076acf73ad60dcb41e63269490cea747c
Jin <rhkdghks21@naver.com>
2024-06-28 19:30:44 +0900

    [FEAT]: Array를 List로 변경했음
    - Bank클래스의 필드를 private필드로 전환
    - Account의 배열 필드를 private필드로 전환

--- bankAccount/src/domain/Account.java
+++ bankAccount/src/domain/Account.java
@@ -1,8 +1,44 @@
 package domain;
+/*
+ * todo
+ * toString 메서드
+ * 코드 테스트와 하는 방법 공부하기
+ * */
+
+import java.util.List;
 
 public class Account {
-    String accountNo;
-    String name;
-    long balance;
-    Transaction transaction;
+
+  // 필드 변수 정의
+  private String accountNo;
+  private String name;
+  private long balance;
+  private List<Transaction> transactions;
+
+  // 게터세터 메서드
+  public String getAccountNo() {
+    return accountNo;
+  }
+
+  public void setAccountNo(String accountNo) {
+    this.accountNo = accountNo;
+  }
+
+  public String getName() {
+    return name;
+  }
+
+  public void setName(String name) {
+    this.name = name;
+  }
+
+  // 과제메서드
+  public void deposit(long amount) {
+
+  }
+
+  public void withdraw(long amount) {
+  }
+
+
 }
