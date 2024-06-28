29ba8f6b0066f5a2417e611f86fef64ec38c6b4a
YangSoeun <soeun8636@naver.com>
2024-06-28 16:54:24 +0900

    [FEAT]: Bank 클래스 생성

---
+++ bankAccount/src/domain/Bank.java
@@ -0,0 +1,36 @@
+package domain;
+
+public class Bank {
+    Account[] accounts;
+    int totalAccount;
+
+    public int getTatalAccount() {
+        return getTatalAccount();
+    }
+    public void setTatalAccount(int tatalAccount) {
+        this.totalAccount = tatalAccount;
+    }
+    public void setAccounts(Account[] accounts) {
+        this.accounts = accounts;
+    }
+    public void addAccount(String accountNo,String name) {
+
+    }
+    public Account getAccount(String accountNo) {
+        return null;
+
+    }
+
+    public Account[] findAccounts(String name) {
+        return accounts;
+
+    }
+    public Account[] getAccounts() {
+        return accounts;
+
+    }
+    public int getTotalAccount() {
+        return totalAccount;
+
+    }
+}
