e92375394e52f380beb719c2ee11f6388a65487b
하민영 <mytokki0323@gmail.com>
2024-06-29 15:21:28 +0900

    [FEAT]:소유자명으로 계좌찾는 메서드(findAccounts)구현

--- bankAccount/src/domain/Bank.java
+++ bankAccount/src/domain/Bank.java
@@ -1,5 +1,6 @@
 package domain;
 
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
 
@@ -34,8 +35,28 @@ public class Bank {
     }
   }
 
-  public List<Account> getAccounts(){
+  public List<Account> getAccounts(){   //계좌 목록 반환 메서드
     return accounts;
+  }
+
+  public List<Account> findAccounts(String name){   //소유자명으로 계좌찾는 메서드
+    List<Account> accountFindByName=new ArrayList<>();
+    if (accounts.isEmpty()) {   //은행에 계좌가 없다면
+      return null;
+    } else {
+      for (Account ac : accounts) {   //은행에 계좌리스트가 있으면 계좌를 돌면서
+        if (ac.getName().equals(name)) {    //해당소유주의 이름과 같은 계좌가 있다면
+          accountFindByName.add(ac);    //accountFindByName리스트에 추가
+        }
+      }
+      if (accountFindByName.isEmpty()){ //accountFindByName리스트가 비어있다면
+        return null;    //널을 반환하고
+      }else{    //accountFindByName리스트에 Account객체가 있다먄
+        return accountFindByName;    //accountFindByName리스트를 반환
+      }
+
+    }
+
   }
 
 }
