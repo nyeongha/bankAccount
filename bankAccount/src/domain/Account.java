e191dd4df9a88c9f327312ab4318647ceb48ab54
Yang-soeun <soeun8636@naver.com>
2024-06-30 17:43:20 +0900

    [FEAT]: 예외 처리 추가

--- bankAccount/src/domain/Account.java
+++ bankAccount/src/domain/Account.java
@@ -1,5 +1,6 @@
 package domain;
 
+import static exception.ErrorMessage.AMOUNT_UNDER_ZERO;
 import static exception.ErrorMessage.NOT_ENOUGH_BALANCE;
 
 import java.util.ArrayList;
@@ -7,66 +8,69 @@ import java.util.List;
 
 public class Account {
 
-  // 필드 변수 정의
-  private String accountNo;
-  private String name;
-  private long balance;
-  private List<Transaction> transactions;
+    // 필드 변수 정의
+    private String accountNo;
+    private String name;
+    private long balance;
+    private List<Transaction> transactions;
 
-  // 생성자 정의 및 기본값 설정
-  public Account(String accountNo, String name) {
-    this.accountNo = accountNo;
-    this.name = name;
-    this.balance = 0L;  // 기본 잔고 0원
-    this.transactions = new ArrayList<Transaction>();
-  }
-
-  public String getAccountNo() {
-    return accountNo;
-  }
-
-  public String getName() {
-    return name;
-  }
-
-  public long getBalance() {
-    return balance;
-  }
-
-  public List<Transaction> getTransactions() {
-       return transactions;
-  }
-
-  public void deposit(long amount) {
-    // 잔고 입금
-    balance += amount;
-
-    // 거래내역 객체 생성 및 필드값 설정
-    Transaction transaction = new Transaction("입금", amount, balance);
-
-    // 거래내역 리스트에 추가
-    transactions.add(transaction);
-  }
-
-  public void withdraw(long amount) {
-    // 잔고 출금
-    if(balance < amount){
-      throw new IllegalArgumentException(NOT_ENOUGH_BALANCE.getMessage());
+    // 생성자 정의 및 기본값 설정
+    public Account(String accountNo, String name) {
+        this.accountNo = accountNo;
+        this.name = name;
+        this.balance = 0L;  // 기본 잔고 0원
+        this.transactions = new ArrayList<Transaction>();
     }
-    balance -= amount;
-    //예외처리 필요할듯
 
-    // 거래내역 객체 생성 및 필드값 설정
-    Transaction transaction = new Transaction("출금", amount, balance);
+    public String getAccountNo() {
+        return accountNo;
+    }
 
-    // 거래내역 리스트에 추가
-    transactions.add(transaction);
-  }
+    public String getName() {
+        return name;
+    }
 
-  @Override
-  public String toString() {
-    return "[계좌번호: " + accountNo+
-            ", 소유자 명: " + name + '\'' +
-            ", 잔액: " + balance +"]";
-  }
+    public long getBalance() {
+        return balance;
+    }
+
+    public List<Transaction> getTransactions() {
+        return transactions;
+    }
+
+    public void deposit(long amount) {
+        if (amount <= 0) {
+            throw new IllegalArgumentException(AMOUNT_UNDER_ZERO.getMessage());
+        }
+        // 잔고 입금
+        balance += amount;
+
+        // 거래내역 객체 생성 및 필드값 설정
+        Transaction transaction = new Transaction("입금", amount, balance);
+
+        // 거래내역 리스트에 추가
+        transactions.add(transaction);
+    }
+
+    public void withdraw(long amount) {
+        // 잔고 출금
+        if (balance < amount) {
+            throw new IllegalArgumentException(NOT_ENOUGH_BALANCE.getMessage());
+        }
+        balance -= amount;
+        //예외처리 필요할듯
+
+        // 거래내역 객체 생성 및 필드값 설정
+        Transaction transaction = new Transaction("출금", amount, balance);
+
+        // 거래내역 리스트에 추가
+        transactions.add(transaction);
+    }
+
+    @Override
+    public String toString() {
+        return "[계좌번호: " + accountNo +
+                ", 소유자 명: " + name +
+                ", 잔액: " + balance + "]";
+    }
 }
--- bankAccount/src/exception/ErrorMessage.java
+++ bankAccount/src/exception/ErrorMessage.java
@@ -1,7 +1,11 @@
 package exception;
 
 public enum ErrorMessage {
-    NOT_ENOUGH_BALANCE("잔액이 부족합니다.");
+    NOT_ENOUGH_BALANCE("잔액이 부족합니다."),
+    ALREADY_EXIST("이미 존재하는 계좌입니다."),
+    NO_SUCH_ACCOUNT("존재하지 않는 계좌입니다."),
+    NO_ACCOUNT("계좌가 존재하지 않습니다."),
+    AMOUNT_UNDER_ZERO("0원 이하는 입금 불가능합니다.");
     private final String message;
 
     ErrorMessage(String message) {
