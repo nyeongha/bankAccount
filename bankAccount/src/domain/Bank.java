89b5e8b4a37bc84e6b8a1bf40735a97655bf7932
minyeong <mytokki0323@gmail.com>
2024-06-30 13:02:07 +0900

    [FIX]: getAccount 예외처리

--- bankAccount/src/domain/Bank.java
+++ bankAccount/src/domain/Bank.java
@@ -2,7 +2,7 @@ package domain;
 
 import java.util.ArrayList;
 import java.util.List;
-import java.util.Optional;
+
 
 public class Bank {
 
@@ -27,14 +27,15 @@ public class Bank {
   }
 
 
-  public Optional<Account> getAccount(String accountNo) { //계좌번호로 계좌찾는 메서드
-
+  public Account getAccount(String accountNo) { //계좌번호로 계좌찾는 메서드
     for (Account account : accounts) {    //accounts에 계좌가 있다면 for문 실행
       if (account.getAccountNo().equals(accountNo)) {   //해당계좌번호가 있다면
-        return Optional.of(account);  //계좌객체 반환
+        return account;
       }
     }
-    return Optional.empty();    //계좌가 없다면 Optional.empty()반환
+    throw new IllegalArgumentException("No such account");
+
+      //계좌가 없다면 Optional.empty()반환
   }
 
 
@@ -42,14 +43,14 @@ public class Bank {
     return accounts;
   }
 
-  public Optional<List<Account>> findAccounts(String name) { // 소유자명으로 계좌 찾는 메서드
+  public List<Account> findAccounts(String name) { // 소유자명으로 계좌 찾는 메서드
     List<Account> accountFindByName = new ArrayList<>();
-    for (Account ac : accounts) { // 은행에 계좌 리스트가 있으면 계좌를 돌면서
-      if (ac.getName().equals(name)) { // 해당 소유주의 이름과 같은 계좌가 있다면
-        accountFindByName.add(ac); // accountFindByName 리스트에 추가
+
+    for (Account account : accounts) { // 은행에 계좌 리스트가 있으면 계좌를 돌면서
+      if (account.getName().equals(name)) { // 해당 소유주의 이름과 같은 계좌가 있다면
+        accountFindByName.add(account); // accountFindByName 리스트에 추가
       }
     }
-    return accountFindByName.isEmpty() ? Optional.empty() : Optional.of(accountFindByName);
+    return accountFindByName;
   }
-
 }
