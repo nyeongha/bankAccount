101db3c73703d2bc3c0f2bc1aa93b56d9b4946b0
minyeong <mytokki0323@gmail.com>
2024-06-30 13:36:48 +0900

    [FEAT]: main 구조 구현

--- bankAccount/src/Main.java
+++ bankAccount/src/Main.java
@@ -1,10 +1,48 @@
+import domain.Account;
+import domain.Bank;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
 public class Main {
+  public static void main(String[] args) throws IOException {
+    Bank bank = new Bank();
+    StringBuilder sb = new StringBuilder();
+    sb.append("1. 전체 계좌 출력\n")
+            .append("2. 계좌로 계좌 정보 찾기\n")
+            .append("3. 소유주명으로 계좌 찾기\n")
+            .append("4. 입금\n")
+            .append("5. 출금\n")
+            .append("6. 거래내역 보기\n")
+            .append("7. 계좌 개설\n")
+            .append("8. 종료");
 
-  public static void main(String[] args) {
+    System.out.println(sb);
+
+    BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+    int menu = Integer.parseInt(br.readLine());
+
+    while (menu != 8){
+      switch (menu){
+        case 1:
+
+        case 2:
+          break;
+        case 3:
+        case 4:
+        case 5:
+        case 6:
+        case 7:
+        default:
 
 
 
 
+
+      }
+    }
 
   }
 }
