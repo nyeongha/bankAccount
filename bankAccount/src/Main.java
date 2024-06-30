2f0e0a56bcbb242d1bece71cffa79843fd36ce74
Jin <rhkdghks21@naver.com>
2024-06-30 14:45:52 +0900

    [FEAT]: main에 계좌생성 및  거래내역 출력기능 구현
    - 계좌번호를 입력하면 계좌를 생성하도록 구현
    - 계좌번호를 입력하면 거래내역을 출력하도록 구현
    - 거래내역이 없으면 "거래내역이 비었습니다."를 출력하고 다시 처음으로 돌아가도록 구현
    - Bank 클래스에 getAccount에 계좌번호 유효성 검사기능 추가

--- bankAccount/src/Main.java
+++ bankAccount/src/Main.java
@@ -1,9 +1,11 @@
 import domain.Account;
 import domain.Bank;
 
+import domain.Transaction;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.List;
 
 public class Main {
   public static void main(String[] args) throws IOException {
@@ -34,7 +36,34 @@ public class Main {
         case 4:
         case 5:
         case 6:
+          System.out.println("계좌번호를 입력해주세요");
+          String excistingAccountNo = br.readLine();
+
+          System.out.println("= 거래내역 =");
+          List<Transaction> transactions = bank.getAccount(excistingAccountNo).getTransactions();
+
+          if (transactions.isEmpty()){
+            System.out.println("거래내역이 비었습니다.");
+          } else{
+            for (Transaction transaction : transactions) {
+              System.out.println(transaction.toString());
+            }
+          }
+
+
+
+
+          break;
         case 7:
+          System.out.println("개설할 계좌번호를 입력해주세요");
+          String newAccountNo = br.readLine();
+
+
+          System.out.println("이름을 입력해주세요");
+          String name = br.readLine();
+
+          bank.addAccount(newAccountNo, name);
+          break;
         default:
 
 
