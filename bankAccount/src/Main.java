086e1ffed95bfdefad15b2b89f1fa33d3c73bfea
minyeong <mytokki0323@gmail.com>
2024-06-30 14:06:46 +0900

    [FEAT]:main클래스 switch 1,2,3 구현

--- bankAccount/src/Main.java
+++ bankAccount/src/Main.java
@@ -4,6 +4,7 @@ import domain.Bank;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.List;
 
 public class Main {
   public static void main(String[] args) throws IOException {
@@ -27,10 +28,25 @@ public class Main {
     while (menu != 8){
       switch (menu){
         case 1:
+          System.out.println("== 전체 계좌 목록 ==");
+          for (Account account: bank.getAccounts()){
+            System.out.printf("[계좌번호: "+ account.getAccountNo()+", 소유자 명: "+account.getName()+", 잔액"+account.getBalance()+"]\n");
+          }
+          break;
 
         case 2:
+          System.out.println("= 해당 계좌번호의 계좌정보 =");
+          Account accountFindByAccountNo = bank.getAccount(br.readLine());
+          System.out.println("[계좌번호 :"+ accountFindByAccountNo.getAccountNo()+", 소유자 명 : "+ accountFindByAccountNo.getName()+", 잔액 : "+ accountFindByAccountNo.getBalance()+"]");
           break;
+
         case 3:
+          System.out.println("= 해당 소유자명의 계좌 목록 =");
+          List<Account> accounts = bank.findAccounts(br.readLine());
+          for (Account accountFindByName:accounts){
+            System.out.println("[계좌번호 :"+accountFindByName.getAccountNo()+", 소유자 명 : "+ accountFindByName.getName()+", 잔액 : "+ accountFindByName.getBalance()+"]");
+          }
+          break;
         case 4:
         case 5:
         case 6:
